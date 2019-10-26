342ab3a011c8446e26def137e2bb71a39d9f264d
unknown <tony31318921@gmail.com>
2019-10-26 15:04:55 +0800

    add refactor

--- products.py
+++ products.py
@@ -1,11 +1,15 @@
-product = []
+products = []
 while True:
 	name = input('請輸入產品名稱: ')
 	if name == 'q':
 		break
 	price = input('請輸入產品價格: ')	
 	p = [name, price]
-	product.append(p)
-print(product)
-for p in product:
+	products.append(p)
+print(products)
+for p in products:
 	print(p[0], '的價格是', p[1])
+
+with open('products.csv', 'w') as f:
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
